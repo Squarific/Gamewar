45ac9da508effee4439cfd172075103157a6cb79
Filip Smets <alemaaltevinden@gmail.com>
2013-12-10 15:11:06 +0100

    Active tabs added

--- client/js/tabview.js
+++ client/js/tabview.js
@@ -1,4 +1,10 @@
 var Tabview = function Tabview (tabcontainer, pagecontainer) {
+	while (tabcontainer.firstChild) {
+		tabcontainer.removeChild(tabcontainer.firstChild);
+	}
+	while (pagecontainer.firstChild) {
+		pagecontainer.removeChild(pagecontainer.firstChild);
+	}
 	this.open = function (title) {
 		var page = document.createElement("div"),
 			tab = document.createElement("div"),
@@ -16,14 +22,21 @@ var Tabview = function Tabview (tabcontainer, pagecontainer) {
 		page.tab = tab;
 		
 		close.addEventListener("click", function (event) {
-			this.parentNode.page.parentNode.removeChild(this.parentNode.page);
-			this.parentNode.parentNode.removeChild(this.parentNode);
-		}, false);
+			var tabContainer = (event.target.parentNode.classList.contains("active")) ? event.target.parentNode.parentNode : {};
+			event.target.parentNode.page.parentNode.removeChild(event.target.parentNode.page);
+			event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+			this.activate(tabContainer.firstChild);
+		}.bind(this), false);
+		
+		tab.addEventListener("click", function (event) {
+			this.activate(event.target);
+		}.bind(this), false);
 		
 		tabcontainer.appendChild(tab);
 		pagecontainer.appendChild(page);
 
 		this.resize();
+		this.activate(tab);
 		
 		return page;
 	};
@@ -34,6 +47,20 @@ var Tabview = function Tabview (tabcontainer, pagecontainer) {
 		for (var page = 0; page < pages.length; page++) {
 			pages[page].style.minHeight = window.innerHeight - pages[page].offsetParent.offsetTop + "px";
 		}
+	};
+	
+	this.activate = function (tab) {
+		if (!tab || !tab.page || !tab.parentNode) {
+			return;
+		}
+		for (var child = 0; child < tab.parentNode.children.length; child++) {
+			if (tab.parentNode.children[child] !== tab) {
+				tab.parentNode.children[child].page.style.zIndex = "";
+				tab.parentNode.children[child].classList.remove("active");
+			}
+		}
+		tab.page.style.zIndex = 999;
+		tab.classList.add("active");
 	};
 	
 	window.addEventListener("resize", this.resize);
